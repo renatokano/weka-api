7098708730b388dc6081851add9399a92f7fffe4
Renato Kano <renato.kenji@hotmail.com>
2015-10-10 08:34:34 -0300

    Added J48 and SMO Classifiers

--- src/weka/api/Classification.java
+++ src/weka/api/Classification.java
@@ -27,6 +27,41 @@ public class Classification {
 		System.out.println(classifier.toString());
 		
 		System.out.println("Finish");
-
+	}
+	
+	public static void j48 (String input) throws Exception {
+		System.out.println("Step 1 - Reading the input file (ARFF Format)");
+		DataSource source = new DataSource(input);
+		Instances dataset = source.getDataSet();
+		dataset.setClassIndex(dataset.numAttributes()-1);
+		
+		System.out.println("Step 2 - Applying the classifier");
+		J48 classifier = new J48();
+		
+		classifier.buildClassifier(dataset);
+		
+		System.out.println("Step 3 - Saving the file (ARFF Format)");
+		System.out.println(classifier.getCapabilities().toString());
+		System.out.println(classifier.toString());
+		
+		System.out.println("Finish");
+	}
+	
+	public static void smo (String input) throws Exception {
+		System.out.println("Step 1 - Reading the input file (ARFF Format)");
+		DataSource source = new DataSource(input);
+		Instances dataset = source.getDataSet();
+		dataset.setClassIndex(dataset.numAttributes()-1);
+		
+		System.out.println("Step 2 - Applying the classifier");
+		SMO classifier = new SMO();
+		
+		classifier.buildClassifier(dataset);
+		
+		System.out.println("Step 3 - Saving the file (ARFF Format)");
+		System.out.println(classifier.getCapabilities().toString());
+		System.out.println(classifier.toString());
+		
+		System.out.println("Finish");
 	}
 }
